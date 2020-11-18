b8bb1179e94dada21ca803f5f2639b9fb3786204
Kayser Ahmed <kayser.ahmed@northsouth.edu>
2020-11-18 09:23:57 +0600

    LAB_2 Assesment Added

---
+++ Lab Assessment Submission/Lab 2/LAB_2_ID_1620252042.asm
@@ -0,0 +1,33 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+; KAYSER AHMED ID:-1620252042
+org 100h
+
+
+ K1 EQU 1
+ K2 EQU 2
+ K3 EQU 3
+ 
+ ADD AX,K1
+ ADD AX,K2
+ ADD AX,K3
+ 
+ 
+ 
+ 
+ 
+ 
+ MOV BX,AX
+ 
+ 
+ 
+ 
+
+
+
+ret
+
+
+
+
